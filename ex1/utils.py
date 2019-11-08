a9298b40b1d18ecaba09e8bff23f34575439a702
Michael Bernreiter <mbernr@outlook.com>
2019-11-08 21:07:48 +0100

    added utils file, included method to determine length of a given path

---
+++ ex1/utils.py
@@ -0,0 +1,8 @@
+
+def get_path_length(G, path):
+	path_length = 0
+	for i in range(len(path)-1):
+		weight = G.get_edge_data(path[i], path[i+1])['weight']
+		print(path[i], path[i+1], weight)
+		path_length += weight
+	return path_length

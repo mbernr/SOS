f4b5b705d58e2c9a0555231fd12ecc4617a58497
Giulio <giulio.pace93@gmail.com>
2019-11-21 14:50:37 +0100

    knapsack genetic

---
+++ ex1/knapsack_genetic.py
@@ -0,0 +1,87 @@
+import random as rn
+import numpy as np
+from utils import *
+from deap import algorithms, base, creator, tools
+
+IND_INIT_SIZE = 5
+MAX_ITEM = 50
+MAX_WEIGHT = 50
+
+items = parse_knapsack("instances/knapsack/low-dimensional/f1_l-d_kp_10_269")
+optimal_sol = parse_knapsack_optimal_solution("instances/knapsack/low-dimensional-optimum/f1_l-d_kp_10_269")
+NBR_ITEMS = items.number_items
+
+
+# To assure reproductibility, the RNG seed is set prior to the items
+# dict initialization.
+rn.seed(64)
+
+creator.create("Fitness", base.Fitness, weights=(-1.0, 1.0))
+creator.create("Individual", set, fitness=creator.Fitness)
+
+toolbox = base.Toolbox()
+
+# Attribute generator
+toolbox.register("attr_item", rn.randrange, NBR_ITEMS)
+
+# Structure initializers
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+    toolbox.attr_item, IND_INIT_SIZE)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def evalKnapsack(individual):
+    weight = 0.0
+    value = 0.0
+    for item in individual:
+        weight += items.weights_items[item]
+        value += items.values_items[item]
+    if len(individual) > MAX_ITEM or weight > MAX_WEIGHT:
+        return 10000, 0             # Ensure overweighted bags are dominated
+    return weight, value
+
+def cxSet(ind1, ind2):
+    """Apply a crossover operation on input sets. The first child is the
+    intersection of the two sets, the second child is the difference of the
+    two sets.
+    """
+    temp = set(ind1)                # Used in order to keep type
+    ind1 &= ind2                    # Intersection (inplace)
+    ind2 ^= temp                    # Symmetric Difference (inplace)
+    return ind1, ind2
+
+def mutSet(individual):
+    """Mutation that pops or add an element."""
+    if rn.random() < 0.5:
+        if len(individual) > 0:     # We cannot pop from an empty set
+            individual.remove(rn.choice(sorted(tuple(individual))))
+    else:
+        individual.add(rn.randrange(NBR_ITEMS))
+    return individual,
+
+toolbox.register("evaluate", evalKnapsack)
+toolbox.register("mate", cxSet)
+toolbox.register("mutate", mutSet)
+toolbox.register("select", tools.selNSGA2)
+
+NGEN = 100 #number of generations
+MU = NBR_ITEMS #numer of individuals to select for next generation
+LAMBDA = NBR_ITEMS*2 #number of children to produce on each generation
+CXPB = 0.7 #probability next generation is produced by crossover
+MUTPB = 0.2 #probability next generation is produced by mutation
+
+pop = toolbox.population(n=MU)
+
+hof = tools.HallOfFame(1)
+
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", np.mean, axis=0)
+stats.register("std", np.std, axis=0)
+stats.register("min", np.min, axis=0)
+stats.register("max", np.max, axis=0)
+
+algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN, stats,
+                          halloffame=hof)
+
+print("Solution found: ", hof)
+print("optimal solution was: ", optimal_sol)
+

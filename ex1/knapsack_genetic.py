03645f140f1536baf4d792ad871e30f2029d973b
Giulio <giulio.pace93@gmail.com>
2019-11-21 15:34:40 +0100

    knapsack genetic

--- ex1/knapsack_genetic.py
+++ ex1/knapsack_genetic.py
@@ -16,7 +16,7 @@ NBR_ITEMS = items.number_items
 # dict initialization.
 rn.seed(64)
 
-creator.create("Fitness", base.Fitness, weights=(-1.0, 1.0))
+creator.create("Fitness", base.Fitness, weights=(1.0,))
 creator.create("Individual", set, fitness=creator.Fitness)
 
 toolbox = base.Toolbox()
@@ -30,14 +30,17 @@ toolbox.register("individual", tools.initRepeat, creator.Individual,
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 def evalKnapsack(individual):
+    print(individual)
     weight = 0.0
     value = 0.0
     for item in individual:
         weight += items.weights_items[item]
         value += items.values_items[item]
     if len(individual) > MAX_ITEM or weight > MAX_WEIGHT:
-        return 10000, 0             # Ensure overweighted bags are dominated
-    return weight, value
+        print("called evalKnapsack on unfeasible solution, returning: 0")
+        return 0,             # Ensure overweighted bags are dominated
+    print("called evalKnapsack, returning: ", value)
+    return value,
 
 def cxSet(ind1, ind2):
     """Apply a crossover operation on input sets. The first child is the
@@ -55,7 +58,9 @@ def mutSet(individual):
         if len(individual) > 0:     # We cannot pop from an empty set
             individual.remove(rn.choice(sorted(tuple(individual))))
     else:
+        print("before: ", individual)
         individual.add(rn.randrange(NBR_ITEMS))
+        print("after: ", individual)
     return individual,
 
 toolbox.register("evaluate", evalKnapsack)
@@ -63,11 +68,11 @@ toolbox.register("mate", cxSet)
 toolbox.register("mutate", mutSet)
 toolbox.register("select", tools.selNSGA2)
 
-NGEN = 100 #number of generations
-MU = NBR_ITEMS #numer of individuals to select for next generation
-LAMBDA = NBR_ITEMS*2 #number of children to produce on each generation
+NGEN = 1000 #number of generations
+MU = 100 #numer of individuals to select for next generation
+LAMBDA = 200 #number of children to produce on each generation
 CXPB = 0.7 #probability next generation is produced by crossover
-MUTPB = 0.2 #probability next generation is produced by mutation
+MUTPB = 0.3 #probability next generation is produced by mutation
 
 pop = toolbox.population(n=MU)
 
@@ -79,8 +84,10 @@ stats.register("std", np.std, axis=0)
 stats.register("min", np.min, axis=0)
 stats.register("max", np.max, axis=0)
 
-algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN, stats,
-                          halloffame=hof)
+#algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN, stats,
+#                          halloffame=hof)
+algorithms.eaSimple(pop, toolbox, 0.7, 0.1, 1000, stats,
+                    halloffame=hof)
 
 print("Solution found: ", hof)
 print("optimal solution was: ", optimal_sol)

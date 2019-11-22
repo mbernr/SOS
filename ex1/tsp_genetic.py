95a6334c6d4fe40a79d92368ca0a593edaa4332a
Michael Bernreiter <mbernr@outlook.com>
2019-11-22 14:13:39 +0100

    looping over all possible parameters

--- ex1/tsp_genetic.py
+++ ex1/tsp_genetic.py
@@ -25,88 +25,94 @@ ind_size = G.number_of_nodes()
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
-
-# Parameters
-population_size = 500
-num_generations = 10
-prb_crossover = 0.6
-prb_mutation = 0.2
-prb_mutation_shuffle = 0.05
-tournament_size = 3
 verbose = False
 
-# Individuals are created as random permutations
-toolbox = base.Toolbox()
-toolbox.register("indices", random.sample, range(ind_size), ind_size)
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-# Specify how individuals will be crossed over, mutated, and selected.
-toolbox.register("mate", tools.cxPartialyMatched)
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=prb_mutation_shuffle)
-toolbox.register("select", tools.selTournament, tournsize=tournament_size)
-toolbox.register("evaluate", evaluate)
-
-# Initialize population.
-#random.seed(169)
-population = toolbox.population(n=population_size)
-
-# Keep the (single) best solution. 
-best_solutions = tools.HallOfFame(1)
-
-# Record statistics.
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", np.mean)
-stats.register("std", np.std)
-stats.register("min", np.min)
-stats.register("max", np.max)
-
-start_time = time.time()
-
-# Execute algorithm provided by DEAP.
-algorithms.eaSimple(
-	population, 
-	toolbox, 
-	prb_crossover, 
-	prb_mutation, 
-	num_generations, 
-	stats=stats, 
-	halloffame=best_solutions,
-	verbose=verbose
-)
-
-end_time = time.time()
-run_time = round(end_time - start_time, 2)
-
-# Format the best solution to always start from vertex 0
-best_solution = start_tour_from_zero(best_solutions[0])
-
-best_solution_value = evaluate(best_solution)[0]
-optimal_solution_value = evaluate(optimal_tour)[0]
-performance = best_solution_value / optimal_solution_value
-
-if verbose:
-	# Print the best solution we found.
-	print()
-	print("Found: {} ({}s)".format(best_solution_value, run_time))
-	print(best_solution)
-
-	# Print the optimal solution.
-	print()
-	print("Optimal: {}".format(optimal_solution_value))
-	print(optimal_tour)
-else:
+if not verbose:
 	print("num_generations, population_size, tournament_size, prb_crossover, prb_mutation, best_found, optimal, performance, run_time")
-	print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}s".format(
-		num_generations,
-		population_size, 
-		tournament_size,
-		prb_crossover, 
-		prb_mutation, 
-		best_solution_value,
-		optimal_solution_value,
-		performance,
-		run_time
-	))
+
+for population_size in [50, 100, 250, 500]:
+	for tournament_size in [3, 10, 20]:
+		for prb in [(0.2,0.7),(0.5,0.5),(0.7,0.2),(0.9,0.1)]: 
+
+			# Parameters
+			# population_size = 500
+			num_generations = 10
+			prb_crossover = prb[0]
+			prb_mutation = prb[1]
+			prb_mutation_shuffle = 0.05
+			tournament_size = 3
+
+			# Individuals are created as random permutations
+			toolbox = base.Toolbox()
+			toolbox.register("indices", random.sample, range(ind_size), ind_size)
+			toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+			toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+			# Specify how individuals will be crossed over, mutated, and selected.
+			toolbox.register("mate", tools.cxPartialyMatched)
+			toolbox.register("mutate", tools.mutShuffleIndexes, indpb=prb_mutation_shuffle)
+			toolbox.register("select", tools.selTournament, tournsize=tournament_size)
+			toolbox.register("evaluate", evaluate)
+
+			# Initialize population.
+			#random.seed(169)
+			population = toolbox.population(n=population_size)
+
+			# Keep the (single) best solution. 
+			best_solutions = tools.HallOfFame(1)
+
+			# Record statistics.
+			stats = tools.Statistics(lambda ind: ind.fitness.values)
+			stats.register("avg", np.mean)
+			stats.register("std", np.std)
+			stats.register("min", np.min)
+			stats.register("max", np.max)
+
+			start_time = time.time()
+
+			# Execute algorithm provided by DEAP.
+			algorithms.eaSimple(
+				population, 
+				toolbox, 
+				prb_crossover, 
+				prb_mutation, 
+				num_generations, 
+				stats=stats, 
+				halloffame=best_solutions,
+				verbose=verbose
+			)
+
+			end_time = time.time()
+			run_time = round(end_time - start_time, 2)
+
+			# Format the best solution to always start from vertex 0
+			best_solution = start_tour_from_zero(best_solutions[0])
+
+			best_solution_value = evaluate(best_solution)[0]
+			optimal_solution_value = evaluate(optimal_tour)[0]
+			performance = best_solution_value / optimal_solution_value
+
+			if verbose:
+				# Print the best solution we found.
+				print()
+				print("Found: {} ({}s)".format(best_solution_value, run_time))
+				print(best_solution)
+
+				# Print the optimal solution.
+				print()
+				print("Optimal: {}".format(optimal_solution_value))
+				print(optimal_tour)
+			else:
+				print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}s".format(
+					num_generations,
+					population_size, 
+					tournament_size,
+					prb_crossover, 
+					prb_mutation, 
+					best_solution_value,
+					optimal_solution_value,
+					performance,
+					run_time
+				))
 
 

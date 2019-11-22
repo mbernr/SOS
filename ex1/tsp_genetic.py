aaa9122b786b3206daea0181d3eb1aed2c3021f2
Michael Bernreiter <mbernr@outlook.com>
2019-11-22 14:05:37 +0100

    printing all necessary paramters

--- ex1/tsp_genetic.py
+++ ex1/tsp_genetic.py
@@ -26,81 +26,87 @@ creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
 
-for i in range(100,1001,100):
+# Parameters
+population_size = 500
+num_generations = 10
+prb_crossover = 0.6
+prb_mutation = 0.2
+prb_mutation_shuffle = 0.05
+tournament_size = 3
+verbose = False
 
-	# Parameters
-	population_size = i
-	num_generations = 1000
-	prb_crossover = 0.75
-	prb_mutation = 0.15
-	prb_mutation_shuffle = 0.05
-	tournament_size = 50
-	verbose = False
+# Individuals are created as random permutations
+toolbox = base.Toolbox()
+toolbox.register("indices", random.sample, range(ind_size), ind_size)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-	# Individuals are created as random permutations
-	toolbox = base.Toolbox()
-	toolbox.register("indices", random.sample, range(ind_size), ind_size)
-	toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
-	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+# Specify how individuals will be crossed over, mutated, and selected.
+toolbox.register("mate", tools.cxPartialyMatched)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=prb_mutation_shuffle)
+toolbox.register("select", tools.selTournament, tournsize=tournament_size)
+toolbox.register("evaluate", evaluate)
 
-	# Specify how individuals will be crossed over, mutated, and selected.
-	toolbox.register("mate", tools.cxPartialyMatched)
-	toolbox.register("mutate", tools.mutShuffleIndexes, indpb=prb_mutation_shuffle)
-	toolbox.register("select", tools.selTournament, tournsize=tournament_size)
-	toolbox.register("evaluate", evaluate)
+# Initialize population.
+#random.seed(169)
+population = toolbox.population(n=population_size)
 
-	# Initialize population.
-	#random.seed(169)
-	population = toolbox.population(n=population_size)
+# Keep the (single) best solution. 
+best_solutions = tools.HallOfFame(1)
 
-	# Keep the (single) best solution. 
-	best_solutions = tools.HallOfFame(1)
+# Record statistics.
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", np.mean)
+stats.register("std", np.std)
+stats.register("min", np.min)
+stats.register("max", np.max)
 
-	# Record statistics.
-	stats = tools.Statistics(lambda ind: ind.fitness.values)
-	stats.register("avg", np.mean)
-	stats.register("std", np.std)
-	stats.register("min", np.min)
-	stats.register("max", np.max)
+start_time = time.time()
 
-	start_time = time.time()
+# Execute algorithm provided by DEAP.
+algorithms.eaSimple(
+	population, 
+	toolbox, 
+	prb_crossover, 
+	prb_mutation, 
+	num_generations, 
+	stats=stats, 
+	halloffame=best_solutions,
+	verbose=verbose
+)
 
-	# Execute algorithm provided by DEAP.
-	algorithms.eaSimple(
-		population, 
-		toolbox, 
+end_time = time.time()
+run_time = round(end_time - start_time, 2)
+
+# Format the best solution to always start from vertex 0
+best_solution = start_tour_from_zero(best_solutions[0])
+
+best_solution_value = evaluate(best_solution)[0]
+optimal_solution_value = evaluate(optimal_tour)[0]
+performance = best_solution_value / optimal_solution_value
+
+if verbose:
+	# Print the best solution we found.
+	print()
+	print("Found: {} ({}s)".format(best_solution_value, run_time))
+	print(best_solution)
+
+	# Print the optimal solution.
+	print()
+	print("Optimal: {}".format(optimal_solution_value))
+	print(optimal_tour)
+else:
+	print("num_generations, population_size, tournament_size, prb_crossover, prb_mutation, best_found, optimal, performance, run_time")
+	print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}s".format(
+		num_generations,
+		population_size, 
+		tournament_size,
 		prb_crossover, 
 		prb_mutation, 
-		num_generations, 
-		stats=stats, 
-		halloffame=best_solutions,
-		verbose=verbose
-	)
-
-	end_time = time.time()
-	run_time = round(end_time - start_time, 2)
-
-	# Format the best solution to always start from vertex 0
-	best_solution = start_tour_from_zero(best_solutions[0])
-
-	if verbose:
-		# Print the best solution we found.
-		print()
-		print("Found: {} ({}s)".format(evaluate(best_solution)[0], run_time))
-		print(best_solution)
-
-		# Print the optimal solution.
-		print()
-		print("Optimal: {}".format(evaluate(optimal_tour)[0]))
-		print(optimal_tour)
-	else:
-		print("{}\t{}\t{}\t{}\t{}\t{}".format(
-			population_size, 
-			prb_crossover, 
-			prb_mutation, 
-			evaluate(best_solution)[0],
-			evaluate(optimal_tour)[0],
-			run_time
-		))
+		best_solution_value,
+		optimal_solution_value,
+		performance,
+		run_time
+	))
 
 

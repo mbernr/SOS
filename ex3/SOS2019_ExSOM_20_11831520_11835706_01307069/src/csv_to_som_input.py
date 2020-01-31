5e0edbc69a6954b213f8fabae3e5f0da4ab15cf1
Michael Bernreiter <mbernr@outlook.com>
2020-01-31 17:48:17 +0100

    added submission folder

---
+++ ex3/SOS2019_ExSOM_20_11831520_11835706_01307069/src/csv_to_som_input.py
@@ -0,0 +1,135 @@
+import csv
+import numpy as np
+from math import sqrt
+import random
+
+
+dataset = "mice"
+column_names = []
+data = []
+target = []
+
+cheat = {
+    'cCSm': 0,
+    'cCSs': 1,
+    'cSCm': 2,
+    'cSCs': 3,
+    'tCSm': 4,
+    'tCSs': 5,
+    'tSCm': 6,
+    'tSCs': 7,
+}
+
+
+# read CSV
+
+with open("data/"+dataset+"/"+dataset+".csv") as file:
+    csv_reader = csv.reader(file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            column_names = row[1:-4]
+            line_count += 1
+        else:
+            data.append(row[1:-4])
+            target.append(row[-1].replace("'", "").replace("-",""))
+            line_count += 1
+
+
+
+# replace missing values
+
+def mean_of_column(data, column):
+    s = 0
+    n = 0
+    for i in range(len(data)):
+        if data[i][column] != '?':
+            s += float(data[i][column])
+            n += 1
+    return s / n
+
+def std_of_column(data, column):
+    temp = [float(data[i][column]) for i in range(len(data))]
+    c = np.array(temp, dtype=float)
+    return np.std(c)
+
+column_means = [mean_of_column(data, i) for i in range(len(data[0]))]
+
+for i in range(len(data)):
+    for column in range(len(data[i])):
+        if data[i][column] == '?':
+            data[i][column] = column_means[column]
+        else:
+            data[i][column] = float(data[i][column])
+
+
+# scale
+
+column_means = [mean_of_column(data, i) for i in range(len(data[0]))]
+column_stds = [std_of_column(data, i) for i in range(len(data[0]))]
+column_rnd = [random.randint(1,101) for i in range(len(data[0]))]
+
+for i in range(len(data)):
+    for column in range(len(data[i])):
+        data[i][column] = (data[i][column] - column_means[column]) / column_stds[column]
+        # data[i][column] *= column_rnd[column]
+        data[i][column] = str(data[i][column])
+
+
+# write .vec file
+
+with open("data/"+dataset+"/"+dataset+".vec", 'w') as file:
+    file.write("$TYPE vec \n")
+    file.write("$XDIM {} \n".format(len(data)))
+    file.write("$YDIM 1 \n")
+    file.write("$VEC_DIM {} \n".format(len(data[0])))
+    for i in range(len(data)):
+        file.write(" ".join(data[i]) + " vec" + str(i+1) + " \n")
+
+
+# write .cls file
+
+with open("data/"+dataset+"/"+dataset+".cls", 'w') as file:
+    file.write("$TYPE class_information \n")
+    file.write("$NUM_CLASSES {} \n".format(len(set(target))))
+    file.write("$CLASS_NAMES {} \n".format(" ".join(sorted(list(set(target))))))
+    file.write("$XDIM 2 \n")
+    file.write("$YDIM {} \n".format(len(target)))
+    for i in range(len(data)):
+        file.write("vec{} {} \n".format(i+1, cheat[target[i]]))
+
+
+#write .tv file
+
+with open("data/"+dataset+"/"+dataset+".tv", 'w') as file:
+    file.write("$TYPE template \n")
+    file.write("$XDIM 2 \n")
+    file.write("$YDIM {} \n".format(len(data)))
+    file.write("$VEC_DIM {} \n".format(len(data[0])))
+    for i in range(len(data[0])):
+        file.write("{} {} \n".format(i, column_names[i]))
+
+
+# write .prop file
+
+with open("data/"+dataset+"/"+dataset+".prop", 'w') as file:
+    file.write("""outputDirectory=../maps/{0} 
+workingDirectory=./
+outputDirectory=../maps/{0}
+namePrefix={0}
+vectorFileName={0}.vec
+sparseData=yes
+isNormalized=yes
+templateFileName={0}.tv
+#cacheSize=
+
+randomSeed=42
+xSize={1}
+ySize={1}
+learnRate=0.75
+sigma=1
+#tau=
+#metricName=
+numIterations={2}
+
+""".format(dataset, round(sqrt(len(data)/10))+5, len(data)*5))
---
+++ ex3/readme.txt
@@ -0,0 +1,12 @@
+SOS Exercise 3, Group 20
+
+This folder contains the data, scripts, and SOMToolbox files used for this project.
+
+The data is contained in mice.csv. See https://www.openml.org/d/40966 for more information on the dataset.
+To generate the files necessary to train a SOM with the SOMToolbox, the csv_to_som_input.py script was used.
+This script generates the files mice.tv, mice.vec, and mice.cls. To experiment with wrongly scaled data,
+the mice_wrongly_scaled.vec file was additionally. generated.
+
+Additionally, the parameter settings for all experiments conducted in this project are contained in
+the respective .prop files, which are numbered by the respective experiment number. 
+

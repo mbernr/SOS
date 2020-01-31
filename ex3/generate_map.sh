a0d1a6d023a4cf2b1755190bb1682e2d1393afcd
Tim Linux <durandtimothee@orange.fr>
2020-01-31 17:36:59 +0100

    added prop files

---
+++ ex3/generate_map.sh
@@ -0,0 +1,3 @@
+somtoolbox/somtoolbox.sh GrowingSOM data/mice/mice08.prop
+
+./somtoolbox/somtoolbox.sh SOMViewer -u data/maps/mice08/mice.unit.gz -w data/maps/mice08/mice.wgt.gz --dw data/maps/mice08/mice.dwm.gz -t data/mice/mice.tv -v data/mice/mice.vec -c data/mice/mice.cls
--- ex3/shit_thirdMap.sh
+++
@@ -1,3 +0,0 @@
-somtoolbox/somtoolbox.sh GrowingSOM data/mice/mice.prop
-
-./somtoolbox/somtoolbox.sh SOMViewer -u data/maps/mice3/mice.unit.gz -w data/maps/mice3/mice.wgt.gz --dw data/maps/mice3/mice.dwm.gz -t data/mice/mice.tv -v data/mice/mice.vec -c data/mice/mice.cls
